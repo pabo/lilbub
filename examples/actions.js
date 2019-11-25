678805e606338c4afe1b973673b92a762f117021
Glitch (hello-express) <none>
2019-11-25 19:06:51 +0000

    📍🐾 Checkpoint ./examples/actions.js:365722/7553 ./package.json:365722/22

---
+++ examples/actions.js
@@ -0,0 +1,85 @@
+// This example shows how to listen to a button click
+// It uses slash commands and actions
+// Require the Bolt package (github.com/slackapi/bolt)
+const { App } = require("@slack/bolt");
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+// Listen for a slash command invocation
+app.command('/helloworld', ({ ack, payload, context }) => {
+  // Acknowledge the command request
+  ack();
+
+  try {
+    const result = app.client.chat.postMessage({
+      token: context.botToken,
+      // Channel to send message to
+      channel: payload.channel_id,
+      // Include a button in the message (or whatever blocks you want!)
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: 'Go ahead. Click it.'
+          },
+          accessory: {
+            type: 'button',
+            text: {
+              type: 'plain_text',
+              text: 'Click me!'
+            },
+            action_id: 'button_abc'
+          }
+        }
+      ],
+      // Text in the notification
+      text: 'Message from Test App'
+    });
+    console.log(result);
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
+
+// Listen for a button invocation with action_id `button_abc` (assume it's inside of a modal)
+app.action('button_abc', ({ ack, payload, action, context }) => {
+  console.log('here');
+  // Acknowledge the button request
+  ack();
+
+  try {
+    // Update the message
+    const result = app.client.chat.update({
+      // ts of message to update
+      ts: action.action_ts,
+      // Channel of message
+      channel: payload.channel.id,
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: '*The button was clicked!*'
+          }
+        }
+      ],
+      text: 'Message from Test App'
+    });
+    console.log(result);
+  }
+  catch (error) {
+    console.error(error);
+  }
+});
+
+(async () => {
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
