b1cee04a5ac0bffe930a630f16fc90b6e699467b
Glitch (hello-express) <none>
2022-08-16 17:28:32 +0000

    ⚡️👀 Checkpoint ./app.js:47711951/4103

--- app.js
+++ app.js
@@ -6,6 +6,33 @@ const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
+
+// const entries = [
+//     {
+//         pattern: /ur[au]gu?ay/i,
+//         response: "no, you're a gay",
+//         percentChance: 100,
+//     },
+//     {
+//         pattern: / gay /i,
+//         response: "I don't know how to tell my parents that I'm gay",
+//         percentChance: 1,
+//     },
+    
+// ];
+
+// for (const entry of entries) {
+//   const {pattern, response, percentChance} = entry;
+//   app.message(pattern, async ({ message, say }) => {
+//     const d100roll = Math.random() * 100; 
+//     console.log("d100roll was", d100roll);
+//     if (d100roll < percentChance) {    
+//       await say(response);
+//     } 
+//   });
+// }
+console.log("starting app")
+
 const gayEmojis = [
   "gayseal",
   "le-gay",
@@ -15,7 +42,7 @@ const gayEmojis = [
 
 //All the room in the world for your code
 app.event('message', async ({ event, client, context }) => {
-  // console.log("event", event);
+  console.log("event", event);
   // console.log("client", client);
   // console.log("context", context);
   
@@ -25,7 +52,7 @@ app.event('message', async ({ event, client, context }) => {
     channel
   } = event;
   
-  if (event.text.match(/ gay /)) {
+  if (event.text.match(/gay/)) {
     await client.reactions.add({
       name: gayEmojis[Math.floor(Math.random()*gayEmojis.length)],
       timestamp: ts,
@@ -33,77 +60,3 @@ app.event('message', async ({ event, client, context }) => {
     })
   }
 });
-
-app.message(/gay/, async ({ message, say }) => {
-  const d100roll = Math.random() * 100; 
-  console.log("d100roll was", d100roll);
-  if (d100roll === 1) {    
-    await say("I don't know how to tell my parents that I'm gay");
-  } 
-});
-
-app.message(/[uU]r[au]gu?ay/i, async ({ message, say }) => {
-    await say("No, you're a gay.");
-});
-
-app.event('app_home_opened', async ({ event, client, context }) => {
-  try {
-    /* view.publish is the method that your app uses to push a view to the Home tab */
-    const result = await client.views.publish({
-
-      /* the user that opened your app's app home */
-      user_id: event.user,
-
-      /* the view object that appears in the app home*/
-      view: {
-        type: 'home',
-        callback_id: 'home_view',
-
-        /* body of the view */
-        blocks: [
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "*Welcome to your _App's Home_* :tada:"
-            }
-          },
-          {
-            "type": "divider"
-          },
-          {
-            "type": "section",
-            "text": {
-              "type": "mrkdwn",
-              "text": "Heya This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
-            }
-          },
-          {
-            "type": "actions",
-            "elements": [
-              {
-                "type": "button",
-                "text": {
-                  "type": "plain_text",
-                  "text": "Click me!"
-                }
-              }
-            ]
-          }
-        ]
-      }
-    });
-  }
-  catch (error) {
-    console.error(error);
-  }
-});
-
-
-
-(async () => {
-  // Start your app
-  await app.start(process.env.PORT || 3000);
-
-  console.log('⚡️ Bolt app is running!');
-})();
